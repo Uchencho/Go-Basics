a525233b3faa065a664b922136a0f5fe7d49e600
Uchencho <aloziekelechi17@gmail.com>
2020-06-06 22:16:08 +0100

    created extra folders

---
+++ Codewars/DuplicateEncoder/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+// func stringInSlice(s string, l []string) bool {
+// 	for _, value := range l {
+// 		if value == s {
+// 			return true
+// 		}
+// 	}
+// 	return false
+// }
+
+func duplicateEncode(word string) string {
+	// f := []string{}
+	for _, val := range word {
+		// res := stringInSlice(val, f)
+		// if res == false {
+		// 	f = append(f, val)
+		// }
+		if val == "h" {
+			fmt.Printf("5c", val)
+		}
+
+	}
+	// fmt.Println(f)
+	return word
+}
+
+func main() {
+	duplicateEncode("Hi there")
+}
